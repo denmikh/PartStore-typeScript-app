5d7a566a46edbf08458ee097629429b40f61f9d4
denmikh <denmih61@gmail.com>
2018-06-15 13:54:24 +0300

    types articles

---
+++ src/models/article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+    id: string,
+    name: string,
+    orig_article: string,
+    description: string,
+    full_description?: string,
+    applicability: string,
+    price: string,
+    img: string
+}
