c6f68f851db2b24673e3cef7779a812d61d323dc
denmikh <denmih61@gmail.com>
2018-08-17 12:26:57 +0300

    workout version on server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+let app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('build'));
+
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT)
+});
+    
